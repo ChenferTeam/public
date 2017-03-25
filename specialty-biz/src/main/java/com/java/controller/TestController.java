4c23f6c659795f380f3bf3b784e483d3b0f1320d
Chenfer <Chenfer@vip.qq.com>
2017-03-25 16:06:02 +0800

    框架

---
+++ specialty-biz/src/main/java/com/java/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.java.controller;
+
+import com.java.service.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Chenfer on 2017/3/25.
+ */
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @RequestMapping(value = {"/test"},method = RequestMethod.GET)
+    public String test(){
+        System.out.println(testService.test());
+        return "index";
+    }
+}
---
+++ specialty-biz/src/main/java/com/java/service/service/TestService.java
@@ -0,0 +1,8 @@
+package com.java.service.service;
+
+/**
+ * Created by Chenfer on 2017/3/25.
+ */
+public interface TestService {
+    String test();
+}
